af2dc5f22c6bc8d808e2ec8d346a60da76875037
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-09 03:27:00 -0400

    Added updateLocalStorage and renderContent classes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,169 @@
 const states = [
-    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
-    'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
-    'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
-    'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
-    'West Virginia', 'Wisconsin', 'Wyoming', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-    'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-    'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-    'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-    'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming",
+  "AL",
+  "AK",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "OH",
+  "OK",
+  "OR",
+  "PA",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
 ];
 
 const cities = [
-    'New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose',
-    'Austin', 'Jacksonville', 'Fort Worth', 'Columbus', 'San Francisco', 'Charlotte', 'Indianapolis', 'Seattle', 'Denver', 'Washington',
-    'Boston', 'El Paso', 'Detroit', 'Nashville', 'Portland', 'Memphis', 'Oklahoma City', 'Las Vegas', 'Louisville', 'Baltimore',
-    'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Mesa', 'Sacramento', 'Atlanta', 'Kansas City', 'Colorado Springs', 'Miami',
-    'Raleigh', 'Omaha', 'Long Beach', 'Virginia Beach', 'Oakland', 'Minneapolis', 'Tampa', 'Tulsa', 'Arlington', 'New Orleans'
+  "New York",
+  "Los Angeles",
+  "Chicago",
+  "Houston",
+  "Phoenix",
+  "Philadelphia",
+  "San Antonio",
+  "San Diego",
+  "Dallas",
+  "San Jose",
+  "Austin",
+  "Jacksonville",
+  "Fort Worth",
+  "Columbus",
+  "San Francisco",
+  "Charlotte",
+  "Indianapolis",
+  "Seattle",
+  "Denver",
+  "Washington",
+  "Boston",
+  "El Paso",
+  "Detroit",
+  "Nashville",
+  "Portland",
+  "Memphis",
+  "Oklahoma City",
+  "Las Vegas",
+  "Louisville",
+  "Baltimore",
+  "Milwaukee",
+  "Albuquerque",
+  "Tucson",
+  "Fresno",
+  "Mesa",
+  "Sacramento",
+  "Atlanta",
+  "Kansas City",
+  "Colorado Springs",
+  "Miami",
+  "Raleigh",
+  "Omaha",
+  "Long Beach",
+  "Virginia Beach",
+  "Oakland",
+  "Minneapolis",
+  "Tampa",
+  "Tulsa",
+  "Arlington",
+  "New Orleans",
 ];
-  
-let weatherData;
 
-function getWeather(cityName, stateName) {
+function getCurrentWeather(cityName, stateName) {
   $.ajax({
-    url: `${window.API_URL}?q=${cityName},${stateName}&appid=${window.API_KEY}&units=imperial`,
+    url: `${window.CURRENT_DAY_API_URL}?q=${cityName},${stateName}&appid=${window.API_KEY}&units=imperial`,
     method: "GET",
     dataType: "json",
   })
 
-    .done(function (responseData) {
-      weatherData = responseData;
-      console.log(weatherData);
+    .done(function (currentWeatherObj) {
+      console.log(currentWeatherObj);
+      return currentWeatherObj;
     })
 
     .fail(function (jqXHR, textStatus, error) {
@@ -38,23 +171,62 @@ function getWeather(cityName, stateName) {
     });
 }
 
+function getFiveDayForecast(cityName, stateName) {
+  $.ajax({
+    url: `${window.FIVE_DAY_API_URL}?q=${cityName},${stateName}&appid=${window.API_KEY}&units=imperial`,
+    method: "GET",
+    dataType: "json",
+  })
 
+    .done(function (fiveDayForecastObj) {
+      console.log(fiveDayForecastObj);
+      return fiveDayForecastObj;
+    })
+
+    .fail(function (jqXHR, textStatus, error) {
+      console.log("Error: " + error);
+    });
+}
+
+function updateLocalStorage(cityName, stateName) {
+  let tempStorage = JSON.parse(localStorage.getItem("locations")) || [];
+
+  let locationExists = tempStorage.some(function () {
+    return location.city === cityName && location.state === stateName;
+  });
+
+  if (!locationExists) {
+    tempStorage.push({ city: cityName, state: stateName });
+    localStorage.setItem("locations", JSON.stringify(tempStorage));
+    renderContent(cityName, stateName);
+  } else {
+    renderContent(cityName, stateName);
+  }
+}
+
+function renderContent(cityName, stateName) {
+  updateLocalStorage(cityName, stateName);
+  let currentWeatherObj = getCurrentWeather(cityName, stateName);
+  let fiveDayForecastObj = getFiveDayForecast(cityName, stateName);
+}
 
 $(document).ready(function () {
-    $("#search").on("click", function (event) {
-        event.preventDefault();
-        const cityName = $("#city-name").val();
-        const stateName = $("#state-name").val();
-        getWeather(cityName, stateName);
-    });
-    
-    $("#city-name").autocomplete({
-        source: cities,
-    });
+  $("#search").on("click", function (event) {
+    event.preventDefault();
+    const cityName =
+      $("#city-name").val().charAt(0).toUpperCase() +
+      $("#city-name").val().slice(1);
+    const stateName =
+      $("#state-name").val().charAt(0).toUpperCase() +
+      $("#state-name").val().slice(1);
+	renderContent(cityName, stateName);
+  });
 
-    $("#state-name").autocomplete({
-        source: states,
-    });
+  $("#city-name").autocomplete({
+    source: cities,
+  });
 
+  $("#state-name").autocomplete({
+    source: states,
+  });
 });
-
