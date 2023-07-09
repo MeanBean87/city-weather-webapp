627bdecaf0593ff29b08746de83398b06a6f12c9
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-09 01:49:37 -0400

    Added city and state arrays.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,35 @@
-function getWeather(cityName, stateCode) {
+const states = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
+    'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+    'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+    'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+    'West Virginia', 'Wisconsin', 'Wyoming', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+    'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+    'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+    'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+    'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
+];
+
+const cities = [
+    'New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose',
+    'Austin', 'Jacksonville', 'Fort Worth', 'Columbus', 'San Francisco', 'Charlotte', 'Indianapolis', 'Seattle', 'Denver', 'Washington',
+    'Boston', 'El Paso', 'Detroit', 'Nashville', 'Portland', 'Memphis', 'Oklahoma City', 'Las Vegas', 'Louisville', 'Baltimore',
+    'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Mesa', 'Sacramento', 'Atlanta', 'Kansas City', 'Colorado Springs', 'Miami',
+    'Raleigh', 'Omaha', 'Long Beach', 'Virginia Beach', 'Oakland', 'Minneapolis', 'Tampa', 'Tulsa', 'Arlington', 'New Orleans'
+];
+  
+let weatherData;
+
+function getWeather(cityName, stateName) {
   $.ajax({
-    url: `${window.API_URL}?q=${cityName},${stateCode}&appid=${window.API_KEY}&units=imperial`,
+    url: `${window.API_URL}?q=${cityName},${stateName}&appid=${window.API_KEY}&units=imperial`,
     method: "GET",
     dataType: "json",
   })
 
-    .done(function (weatherData) {
-      responseData = weatherData;
+    .done(function (responseData) {
+      weatherData = responseData;
       console.log(weatherData);
     })
 
@@ -15,11 +38,23 @@ function getWeather(cityName, stateCode) {
     });
 }
 
-let responseData;
 
-$("#search").on("click", function (event) {
-  event.preventDefault();
-  const cityName = $("#city-name").val();
-  const stateCode = $("#state-name").val();
-  getWeather(cityName, stateCode);
+
+$(document).ready(function () {
+    $("#search").on("click", function (event) {
+        event.preventDefault();
+        const cityName = $("#city-name").val();
+        const stateName = $("#state-name").val();
+        getWeather(cityName, stateName);
+    });
+    
+    $("#city-name").autocomplete({
+        source: cities,
+    });
+
+    $("#state-name").autocomplete({
+        source: states,
+    });
+
 });
+
