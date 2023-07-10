5c39160ec9c0ce508da4374fe48c5ac49a49d26e
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-10 10:02:31 -0400

    updated renderContent to target lon/lat API endpoint.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,241 +1,159 @@
-const states = [
-  "Alabama",
-  "Alaska",
-  "Arizona",
-  "Arkansas",
-  "California",
-  "Colorado",
-  "Connecticut",
-  "Delaware",
-  "Florida",
-  "Georgia",
-  "Hawaii",
-  "Idaho",
-  "Illinois",
-  "Indiana",
-  "Iowa",
-  "Kansas",
-  "Kentucky",
-  "Louisiana",
-  "Maine",
-  "Maryland",
-  "Massachusetts",
-  "Michigan",
-  "Minnesota",
-  "Mississippi",
-  "Missouri",
-  "Montana",
-  "Nebraska",
-  "Nevada",
-  "New Hampshire",
-  "New Jersey",
-  "New Mexico",
-  "New York",
-  "North Carolina",
-  "North Dakota",
-  "Ohio",
-  "Oklahoma",
-  "Oregon",
-  "Pennsylvania",
-  "Rhode Island",
-  "South Carolina",
-  "South Dakota",
-  "Tennessee",
-  "Texas",
-  "Utah",
-  "Vermont",
-  "Virginia",
-  "Washington",
-  "West Virginia",
-  "Wisconsin",
-  "Wyoming",
-  "AL",
-  "AK",
-  "AZ",
-  "AR",
-  "CA",
-  "CO",
-  "CT",
-  "DE",
-  "FL",
-  "GA",
-  "HI",
-  "ID",
-  "IL",
-  "IN",
-  "IA",
-  "KS",
-  "KY",
-  "LA",
-  "ME",
-  "MD",
-  "MA",
-  "MI",
-  "MN",
-  "MS",
-  "MO",
-  "MT",
-  "NE",
-  "NV",
-  "NH",
-  "NJ",
-  "NM",
-  "NY",
-  "NC",
-  "ND",
-  "OH",
-  "OK",
-  "OR",
-  "PA",
-  "RI",
-  "SC",
-  "SD",
-  "TN",
-  "TX",
-  "UT",
-  "VT",
-  "VA",
-  "WA",
-  "WV",
-  "WI",
-  "WY",
-];
+const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Louisville"];
 
-const cities = [
-  "New York",
-  "Los Angeles",
-  "Chicago",
-  "Houston",
-  "Phoenix",
-  "Philadelphia",
-  "San Antonio",
-  "San Diego",
-  "Dallas",
-  "San Jose",
-  "Austin",
-  "Jacksonville",
-  "Fort Worth",
-  "Columbus",
-  "San Francisco",
-  "Charlotte",
-  "Indianapolis",
-  "Seattle",
-  "Denver",
-  "Washington",
-  "Boston",
-  "El Paso",
-  "Detroit",
-  "Nashville",
-  "Portland",
-  "Memphis",
-  "Oklahoma City",
-  "Las Vegas",
-  "Louisville",
-  "Baltimore",
-  "Milwaukee",
-  "Albuquerque",
-  "Tucson",
-  "Fresno",
-  "Mesa",
-  "Sacramento",
-  "Atlanta",
-  "Kansas City",
-  "Colorado Springs",
-  "Miami",
-  "Raleigh",
-  "Omaha",
-  "Long Beach",
-  "Virginia Beach",
-  "Oakland",
-  "Minneapolis",
-  "Tampa",
-  "Tulsa",
-  "Arlington",
-  "New Orleans",
-];
+async function getCityInfo(cityName) {
+  return $.ajax({
+    url: `${window.GEOCODING_API_URL}?q=${cityName}&appid=${window.API_KEY}`,
+    method: "GET",
+    dataType: "json",
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log("Error:", textStatus, errorThrown);
+  });
+}
 
-let currentWeatherObj = {};
-let fiveDayForecastObj = {};
+async function getCurrentWeatherCoordinates(lat, lon) {
+  return $.ajax({
+    url: `${window.CURRENT_DAY_API_URL}?lat=${lat}&lon=${lon}&appid=${window.API_KEY}&units=imperial`,
+    method: "GET",
+    dataType: "json",
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log("Error:", textStatus, errorThrown);
+  });
+}
 
-function getCurrentWeather(cityName, stateName) {
-	return $.ajax({
-	  url: `${window.CURRENT_DAY_API_URL}?q=${cityName},${stateName}&appid=${window.API_KEY}&units=imperial`,
-	  method: "GET",
-	  dataType: "json",
-	});
-  }
+async function getFiveDayForecastCoordinates(lat, lon) {
+  return $.ajax({
+    url: `${window.FIVE_DAY_API_URL}?lat=${lat}&lon=${lon}&appid=${window.API_KEY}&units=imperial`,
+    method: "GET",
+    dataType: "json",
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log("Error:", textStatus, errorThrown);
+  });
+}
 
-  function getFiveDayForecast(cityName, stateName) {
-	return $.ajax({
-	  url: `${window.FIVE_DAY_API_URL}?q=${cityName},${stateName}&appid=${window.API_KEY}&units=imperial`,
-	  method: "GET",
-	  dataType: "json",
-	});
-  }
+async function getCurrentWeather(cityName) {
+  return $.ajax({
+    url: `${window.CURRENT_DAY_API_URL}?q=${cityName}&appid=${window.API_KEY}&units=imperial`,
+    method: "GET",
+    dataType: "json",
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log("Error:", textStatus, errorThrown);
+  });
+}
+
+async function getFiveDayForecast(cityName) {
+  return $.ajax({
+    url: `${window.FIVE_DAY_API_URL}?q=${cityName}&appid=${window.API_KEY}&units=imperial`,
+    method: "GET",
+    dataType: "json",
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log("Error:", textStatus, errorThrown);
+  });
+}
 
 function updateLocalStorage(cityName, stateName) {
-	let tempStorage = JSON.parse(localStorage.getItem("locations"));
-  
-	if (!Array.isArray(tempStorage)) {
-	  tempStorage = [];
-	}
-  
-	let locationExists = false;
-  
-	for (const location of tempStorage) {
-	  if (location.city === cityName && location.state === stateName) {
-		locationExists = true;
-		break;
-	  }
-	}
-  
-	if (!locationExists) {
-	  tempStorage.push({ city: cityName, state: stateName });
-	  localStorage.setItem("locations", JSON.stringify(tempStorage));
-	  renderContent(cityName, stateName);
-	} else {
-	  renderContent(cityName, stateName);
-	}
+  let tempStorage = JSON.parse(localStorage.getItem("locations"));
+
+  if (!Array.isArray(tempStorage)) {
+    tempStorage = [];
   }
 
-  function renderContent(cityName, stateName) {
-	getCurrentWeather(cityName, stateName)
-	  .done(function (currentWeatherObj) {
-		getFiveDayForecast(cityName, stateName)
-		  .done(function (fiveDayForecastObj) {
-			createElements(currentWeatherObj, fiveDayForecastObj);
-		  })
-		  .fail(function (jqXHR, textStatus, error) {
-			console.log("Error: " + error);
-		  });
-	  })
-	  .fail(function (jqXHR, textStatus, error) {
-		console.log("Error: " + error);
-	  });
+  let locationExists = false;
+
+  for (const location of tempStorage) {
+    if (location.city === cityName && location.state === stateName) {
+      locationExists = true;
+      break;
+    }
   }
 
-function createElements(currentWeatherObj, fiveDayForecastObj) {
+  if (!locationExists) {
+    tempStorage.push({ city: cityName, state: stateName });
+    localStorage.setItem("locations", JSON.stringify(tempStorage));
+  }
+}
+
+async function renderContent(cityName) {
+  let cityObj = await getCityInfo(cityName);
+  let resultIndex = searchResults(cityName, cityObj);
+
+  if (resultIndex !== -1) {
+    let currentWeatherObj = await getCurrentWeatherCoordinates(
+      cityObj[resultIndex].lat,
+      cityObj[resultIndex].lon
+    );
+    let fiveDayForecastObj = await getFiveDayForecastCoordinates(
+      cityObj[resultIndex].lat,
+      cityObj[resultIndex].lon
+    );
+    console.log("Pulling with Coords");
+    createElements(currentWeatherObj, fiveDayForecastObj, cityObj, cityName);
+  } else {
+    let currentWeatherObj = await getCurrentWeather(cityName);
+    let fiveDayForecastObj = await getFiveDayForecast(cityName);
+    console.log("Pulling with City Name");
+    createElements(currentWeatherObj, fiveDayForecastObj, cityObj, cityName);
+  }
+}
+function removeSpecialCharacters(string) {
+  return string.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase();
+}
+
+function searchResults(string, array) {
+  let stringArray = removeSpecialCharacters(string).split(" ");
+  let resultIndex = -1;
+
+  for (let i = 0; i < array.length; i++) {
+    if (stringArray.includes(array[i].state.toLowerCase())) {
+      resultIndex = i;
+      break;
+    }
+  }
+  return resultIndex;
+}
+
+function createElements(
+  currentWeatherObj,
+  fiveDayForecastObj,
+  cityObj,
+  cityName
+) {
+  let index = searchResults(cityName, cityObj);
+  let city = "";
+  let state = "";
+  if (index === -1) {
+    city = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+  } else {
+    city = cityObj[index].name;
+    state = cityObj[index].state;
+  }
+
   $(".current-weather").append(
-    `<h2>${currentWeatherObj.name} ${dayjs.unix(currentWeatherObj.dt).format("MM/DD/YYYY")}</h2>
-		<img src="${`http://openweathermap.org/img/wn/${currentWeatherObj.weather[0].icon}@2x.png`}"></img>
+    `<h2>${city}, ${state} ${dayjs
+      .unix(currentWeatherObj.dt)
+      .format("MM/DD/YYYY")}
+	<img src="${`http://openweathermap.org/img/wn/${currentWeatherObj.weather[0].icon}@2x.png`}"></img></h2>
 		<p>Temperature: ${currentWeatherObj.main.temp}°F</p>
 		<p>Wind: ${currentWeatherObj.wind.speed}MPH</p>
 		<p>Humidity: ${currentWeatherObj.main.humidity}%</p>`
   );
 
-	let storedIndexes = [];
-	
-	for (let i = 0; i < fiveDayForecastObj.list.length; i++) {
-		if (fiveDayForecastObj.list[i].dt_txt.includes("12:00:00")) {
-			storedIndexes.push(i);
-		}
-	}
-	
+  let storedIndexes = [];
+
+  for (let i = 0; i < fiveDayForecastObj.list.length; i++) {
+    if (fiveDayForecastObj.list[i].dt_txt.includes("12:00:00")) {
+      storedIndexes.push(i);
+    }
+  }
+
   for (let i = 0; i < storedIndexes.length; i++) {
     $(".five-day-forecast").append(
       `<card id="day-${[i]}">
-		  <h3>${dayjs.unix(fiveDayForecastObj.list[storedIndexes[i]].dt).format("MM/DD/YYYY")}</h3>
-		  <img src="${`http://openweathermap.org/img/wn/${fiveDayForecastObj.list[storedIndexes[i]].weather[0].icon}@2x.png`}"></img>
+		  <h3>${dayjs
+        .unix(fiveDayForecastObj.list[storedIndexes[i]].dt)
+        .format("MM/DD/YYYY")}</h3>
+		  <img src="${`http://openweathermap.org/img/wn/${
+        fiveDayForecastObj.list[storedIndexes[i]].weather[0].icon
+      }@2x.png`}"></img>
 		  <p>Temperature: ${fiveDayForecastObj.list[storedIndexes[i]].main.temp}°F</p>
 		  <p>Wind: ${fiveDayForecastObj.list[storedIndexes[i]].wind.speed}MPH</p>
 		  <p>Humidity: ${fiveDayForecastObj.list[storedIndexes[i]].main.humidity}%</p>`
@@ -246,22 +164,35 @@ function createElements(currentWeatherObj, fiveDayForecastObj) {
 $(document).ready(function () {
   $("#search").on("click", function (event) {
     event.preventDefault();
-    const cityName =
-      $("#city-name").val().charAt(0).toUpperCase() +
-      $("#city-name").val().slice(1);
-    const stateName =
-      $("#state-name").val().charAt(0).toUpperCase() +
-      $("#state-name").val().slice(1);
-	updateLocalStorage(cityName, stateName);
+    $(".current-weather").empty();
+    $(".five-day-forecast").empty();
+    const cityName = $("#city-name").val().toLowerCase();
+    renderContent(cityName);
   });
-
-  $("#city-name").autocomplete({
-    source: cities,
-  });
-
-  $("#state-name").autocomplete({
-    source: states,
-  });
-
 });
 
+async function getCityById(cityId) {
+  return new Promise(function (resolve, reject) {
+    const url = `https://meanbean87.github.io/city-list-API/city.list.min.json`;
+    $.getJSON(url, function (data) {
+      const city = data.find((city) => city.id === cityId);
+      if (city) {
+        resolve(city);
+      } else {
+        reject("City not found");
+      }
+    }).fail(function (jqxhr, textStatus, error) {
+      console.log("Error:", textStatus, error);
+      reject(error);
+    });
+  });
+}
+
+const cityId = 4299276;
+getCityById(cityId)
+  .then(function (city) {
+    console.log(city);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
